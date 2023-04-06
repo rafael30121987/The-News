ef4ca4fd03ee7d1981559e86a31ac8f4d52cc0c5
Danilo Eduardo Moreira <114699259+rafael30121987@users.noreply.github.com>
2023-04-06 10:54:29 -0300

    showing the news

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 const NewsAPI = require('newsapi')
+const $ = require('jquery')
 const newsapi = new NewsAPI('0e38e59576064d918a5e1dcb51498fa2')
 
 newsapi.v2.topHeadlines({
     category: 'business',
     language: 'en',
     country: 'us'
-}).then((results)=>{
+}).then((results) => {
     console.log(results)
     showNews(results.articles)
-}).catch((err)=>{
+}).catch((err) => {
     console.log(err)
 })
 
 
-function showNews(allNews){
+function showNews(allNews) {
+    allNews.forEach(news => {
+        
+        let singleNews =`
+        <li class="list-group-item">
+            <img class="img-circle media-object pull-left" src="${news.urlToImage}" width="50"
+                height="50">
+                <div class="media-body">
+                    <strong><a href="" >${news.title}</a></strong>
+                    <div>
+                        <span class="">${news.publishedAt}</span>
+                        <span class="pull-right">${news.author}</span>
+                    </div>
+                    <p>${news.description}</p>
+                </div>
+        </li>`
 
+        $('#news-list').append(singleNews)
+    });
 }
